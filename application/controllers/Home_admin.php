e69442b9c3569157582939b6dba08619ccc9d5ea
Khalil Attalla Firdia <khalilattalla2209@gmail.com>
2021-03-18 09:55:19 +0700

    penambahan folder update dan tambah di mode admin

--- application/controllers/Home_admin.php
+++ application/controllers/Home_admin.php
@@ -18,70 +18,70 @@ class Home_admin extends CI_Controller
     public function manage_menu()
     {
         $this->load->view('admin/template/header');
-        $this->load->view('admin/manage_menu');
+        $this->load->view('admin/manage/manage_menu');
         $this->load->view('admin/template/footer');
     }
 
     public function manage_instansi()
     {
         $this->load->view('admin/template/header');
-        $this->load->view('admin/manage_instansi');
+        $this->load->view('admin/manage/manage_instansi');
         $this->load->view('admin/template/footer');
     }
 
     public function manage_user()
     {
         $this->load->view('admin/template/header');
-        $this->load->view('admin/manage_user');
+        $this->load->view('admin/manage/manage_user');
         $this->load->view('admin/template/footer');
     }
 
     public function manage_category()
     {
         $this->load->view('admin/template/header');
-        $this->load->view('admin/manage_category');
+        $this->load->view('admin/manage/manage_category');
         $this->load->view('admin/template/footer');
     }
 
     public function manage_article_news()
     {
         $this->load->view('admin/template/header');
-        $this->load->view('admin/manage_article_news');
+        $this->load->view('admin/manage/manage_article_news');
         $this->load->view('admin/template/footer');
     }
 
     public function manage_article_upload()
     {
         $this->load->view('admin/template/header');
-        $this->load->view('admin/manage_article_upload');
+        $this->load->view('admin/manage/manage_article_upload');
         $this->load->view('admin/template/footer');
     }
 
     public function manage_slide_show()
     {
         $this->load->view('admin/template/header');
-        $this->load->view('admin/manage_slide_show');
+        $this->load->view('admin/manage/manage_slide_show');
         $this->load->view('admin/template/footer');
     }
 
     public function manage_page_news()
     {
         $this->load->view('admin/template/header');
-        $this->load->view('admin/manage_page_news');
+        $this->load->view('admin/manage/manage_page_news');
         $this->load->view('admin/template/footer');
     }
 
     public function manage_photo()
     {
         $this->load->view('admin/template/header');
-        $this->load->view('admin/manage_photo');
+        $this->load->view('admin/manage/manage_photo');
         $this->load->view('admin/template/footer');
     }
 
     public function manage_video()
     {
         $this->load->view('admin/template/header');
-        $this->load->view('admin/manage_video');
+        $this->load->view('admin/manage/manage_video');
         $this->load->view('admin/template/footer');
     }
 }
