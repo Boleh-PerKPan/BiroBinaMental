6328309853d24458c66089f0603e157106ec179f
Khalil Attalla Firdia <khalilattalla2209@gmail.com>
2021-03-23 09:28:55 +0700

    hello world

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,36 +1,34 @@
-
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-        <h1 class="h2">Dashboard</h1>
-        <div class="btn-toolbar mb-2 mb-md-0">
-        </div>
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+    <h1 class="h2">Dashboard</h1>
+    <div class="btn-toolbar mb-2 mb-md-0">
     </div>
+</div>
 
 
-    <div class="row">
-        <div class="col-md-2">
-            <div class="card" style="height: 120px;">
-                <div class="card-body">
-                    <h5><i class="fas fa-tachometer-alt" style="font-size: 2.5em;"></i></h5>
-                    <h5>Dashboard</h5>
-                </div>
+<div class="row">
+    <div class="col-md-2">
+        <a class="card card-link" href="#" style="height: 120px;">
+            <div class="card-body">
+                <h5><i class="fas fa-tachometer-alt" style="font-size: 2.5em; "></i></h5>
+                <h5>Dashboard</h5>
             </div>
-        </div>
-        <div class=" col-md-2">
-            <div class="card" style="height: 120px;">
-                <div class="card-body">
-                    <h5><i class="fas fa-user" style="font-size: 2.5em;"></i> </h5>
-                    <h5>User</h5>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="card" style="height: 120px;">
-                <div class="card-body">
-                    <h5><i class="fas fa-sign-out-alt" style="font-size: 2.5em;"></i></h5>
-                    <h5>Logout</h5>
-                </div>
-            </div>
-        </div>
+        </a>
     </div>
+    <div class=" col-md-2">
+        <a class="card card-link" href="#" style="height: 120px;">
+            <div class="card-body">
+                <h5><i class="fas fa-user" style="font-size: 2.5em;"></i> </h5>
+                <h5>User</h5>
+            </div>
+        </a>
     </div>
-
+    <div class="col-md-2">
+        <a class="card card-link" href="#" style="height: 120px;">
+            <div class="card-body">
+                <h5><i class="fas fa-sign-out-alt" style="font-size: 2.5em;"></i></h5>
+                <h5>Logout</h5>
+            </div>
+        </a>
+    </div>
+</div>
+</div>
