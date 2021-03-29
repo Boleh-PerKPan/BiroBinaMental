ab909b6f075c25dfe6bd102057e964e6d21cf55d
iffatuzzahra <iffa_zahra@yahoo.com>
2021-03-29 10:05:20 +0700

    detailpage added

---
+++ application/views/guest/detail.php
@@ -0,0 +1,39 @@
+        <div class="new-panel">
+            <div>
+                <a href="<?= base_url()?>guest/index_berita"><h4 class="nav-style">Berita Utama</h4></a>
+            </div>
+            <div class="card mb-3 new-panel">
+                <img src="<?= base_url() ?>assets/files/images/pendd_char.png" class="card-img-top w-75 m-auto mt-lg-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p style="text-align:right" class="article-item">
+                        <i class="fas fa-tag"></i> Berita Utama &nbsp; &nbsp;
+                        <i class="fas fa-user"></i> Dipublish oleh &nbsp; &nbsp;
+                        <i class="fas fa-calendar-alt"></i> waktu publish &nbsp; &nbsp;
+                    </p>
+                    <p class="card-text">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores ipsa magnam odit atque optio nihil perspiciatis, officia quos. Vitae assumenda laboriosam corporis! Neque vero quidem placeat repudiandae nam numquam delectus.
+                        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer. 
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores ipsa magnam odit atque optio nihil perspiciatis, officia quos. Vitae assumenda laboriosam corporis! Neque vero quidem placeat repudiandae nam numquam delectus.
+                        This is a wider card with supporting text below as a nat<br>ural lead-in to additional content. This content is a little bit longer
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores ipsa magnam odit atque optio nihil perspiciatis, officia quos. Vitae assumenda laboriosam corporis! Neque vero quidem placeat repudiandae nam numquam delectus.
+                        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores ipsa magnam odit atque optio nihil perspiciatis, officia quos. Vitae assumenda laboriosam corporis! Neque vero quidem placeat repudiandae nam numquam delectus.
+                        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor <br>es ipsa magnam odit atque optio nihil perspiciatis, officia quos. Vitae assumenda laboriosam corporis! Neque vero quidem placeat repudiandae nam numquam delectus.
+                        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a <br>little bit longer
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores ipsa magnam odit atque optio nihil perspiciatis, officia quos. Vitae assumenda laboriosam corporis! Neque vero quidem placeat repudiandae nam numquam delectus.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores ipsa magnam odit atque optio nihil perspiciatis, officia quos. Vitae assumenda laboriosam corporis! Neque vero quidem placeat repudiandae nam numquam delectus.
+                        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longerThis is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="new-panel" id="beritaterkait" >
+            <h4 class="nav-style">Berita Terkait</h4>
+            <div>
+                <a href="<?=base_url()?>/guest/extrapage_news/sejarah" class="btn bg-transparent" style="color:black"><i class="fas fa-chevron-right"></i> Sejarah</a> <hr>
+                <a href="<?=base_url()?>/guest/extrapage_news/struktur_organisasi" class="btn bg-transparent" style="color:black"><i class="fas fa-chevron-right"></i> Struktur Organisasi</a> <hr>  
+                <a href="<?=base_url()?>/guest/extrapage_news/ProfilePejabat" class="btn bg-transparent" style="color:black"><i class="fas fa-chevron-right"></i> Profile Pejabat </a> <hr>           
+            </div>
+        </div>
