93405edbc5d2c0930c95e0780f0286631c9905d6
Khalil Attalla Firdia <khalilattalla2209@gmail.com>
2021-04-12 09:40:44 +0700

    push terakhir dariku

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,10 +1,10 @@
 <main role="main" class="container jumbotron bg-transparent a">
     <?= $this->session->flashdata('message3') ?>
     <?php $this->session->unset_userdata('message3'); ?>
-    <div class="col-md-4 mx-auto" style="margin-top: 100px;">
+    <div class="col-md-4 mx-auto" style="margin-top: 80px;">
         <div class="card">
             <div class="">
-                <img src="<?= base_url() ?>assets/img/dont-delete-this-folder/sumbarprof_logo.ico" alt="Gambar login" class="" style="margin-top:10px ; height:50px; width: 50px; ">
+                <img src="<?= base_url() ?>assets/img/dont-delete-this-folder/sumbarprof_logo.ico" alt="Gambar login" class="" style="margin-top:30px ; margin-bottom: 10px; height:100px; width: 100px; ">
             </div>
             <div class="card-body">
                 <form method="POST">
@@ -22,7 +22,7 @@
                         <?php $this->session->unset_userdata('message2'); ?>
                         <!-- <small class="form-text text-danger">Password Wajib Diisi</small> -->
                     </div>
-                    <button type="submit" class="btn btn-primary">Login</button>
+                    <button type="submit" class="btn btn-primary form-control" style="margin-bottom: 20px;">Login</button>
                 </form>
             </div>
         </div>
